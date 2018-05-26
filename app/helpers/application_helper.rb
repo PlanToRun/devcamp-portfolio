b21b83c33eaf92a563660cf66086da2942e80c3e
moira <moira.sanna@gmail.com>
2018-05-26 12:48:42 +0100

    Integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def copyright_generator
+    SannaViewTool::Renderer.copyright 'Moira Sanna', 'All rights reserved'
+  end
 end
